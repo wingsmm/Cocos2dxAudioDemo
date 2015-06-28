eeeefd9ba6630f90112edfa90e1ee82117347776
wingsmm <wingsmm@me.com>
2015-06-28 09:33:01 +0800

    no message

---
+++ CocosDenshion/Classes/AppDelegate.cpp
@@ -0,0 +1,82 @@
+#include "AppDelegate.h"
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLViewImpl::createWithRect("CocosDenshion", Rect(0, 0, 960, 640));
+        director->setOpenGLView(glview);
+    }
+
+
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+
+    FileUtils::getInstance()->addSearchPath("res");
+
+//    // create a scene. it's an autorelease object
+//    auto scene = HelloWorld::createScene();
+//
+//    // run
+//    director->runWithScene(scene);
+    
+ 
+    auto screenSize = glview->getFrameSize();
+    auto designSize = Size(480, 320);
+    
+    glview->setDesignResolutionSize(designSize.width, designSize.height, ResolutionPolicy::SHOW_ALL);
+
+    
+    _testController = TestController::getInstance();
+
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground()
+{
+    if (_testController)
+    {
+        _testController->onEnterBackground();
+    }
+    
+    Director::getInstance()->stopAnimation();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground()
+{
+    if (_testController)
+    {
+        _testController->onEnterForeground();
+    }
+    
+    Director::getInstance()->startAnimation();
+}
